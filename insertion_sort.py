838993e052eac4aad89ab74b905e3937e2fe27fb
Rafael Gondo <rafael@rafael-notebook.(none)>
2013-03-25 17:47:20 -0300

    2

--- insertion_sort.py
+++
@@ -1,9 +0,0 @@
-def insertion_sort(vetor):
-	for i in range(1,len(vetor)):
-		x = vetor[i]
-		j = i-1
-		while (j >= 0) and (vetor[j]>x):
-			vetor[j+1] = vetor[j]
-			j -= 1
-		vetor[j+1] = x
-	print vetor
